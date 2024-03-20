5ae034ffc561d81dea4778f8827b7b06ebb7a4d9
alex <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-03-20 21:47:49 +0000

    feat: Added new elements  and styles.css, fixed a typo and added new HTML files for different recipes

--- upgrades.js
+++ upgrades.js
@@ -6,3 +6,11 @@ clicker.addEventListener("click", function () {
 clicker.addEventListener("animationend", function () {
   clicker.classList.remove("click-animation");
 });
+let clicker2 = document.getElementById("upgrade1-scret");
+clicker2.addEventListener("click", function () {
+  clicker2.classList.add("click-animation");
+});
+
+clicker2.addEventListener("animationend", function () {
+  clicker2.classList.remove("click-animation");
+});
