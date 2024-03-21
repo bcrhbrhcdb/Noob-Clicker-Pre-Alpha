616dc98a10b7eb6bad972c6c77e2be671c2a7ac5
alex <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-03-21 19:13:37 +0000

    feat: Added new HTML file for Browsersync UI

---
+++ game.js
@@ -0,0 +1,7 @@
+const switchClicker = document.getElementById("upgrade1-secret-2");
+switchClicker.addEventListener("click", () => {
+  const clickerOne = document.getElementById("img");
+  const clickerTwo = document.getElementById("upgrade1-secret");
+  clickerOne.style.display = "none"; // or 'block', depends on your needs
+  clickerTwo.style.display = "block";
+});
