85a90d57aac548db3efc48545dda45af4d3c47be
alex <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-03-22 15:24:38 +0000

    n

--- game.js
+++ game.js
@@ -1,8 +1,6 @@
-// switch clicker stuff
 const switchClicker = document.getElementById("upgrade1-secret-2");
 let clickerOne = document.getElementById("img");
 let clickerTwo = document.getElementById("upgrade1-secret");
-
 switchClicker.addEventListener("click", function () {
   if (clickerOne.style.display === "none") {
     clickerOne.style.display = "block";
