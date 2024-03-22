1e127c9fbea01bcfbbab38a7875299225ffd14ea
alex <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-03-22 13:50:40 +0000

    n

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
+// switch clicker stuff
 const switchClicker = document.getElementById("upgrade1-secret-2");
-switchClicker.addEventListener("click", () => {
-  const clickerOne = document.getElementById("img");
-  const clickerTwo = document.getElementById("upgrade1-secret");
-  clickerOne.style.display = "none"; // or 'block', depends on your needs
-  clickerTwo.style.display = "block";
-  if (switchClicker) {
-    switchClicker.addEventListener("click", () => {
-      clickerOne.style.display = "block";
-      clickerTwo.style.display = "none;";
-    });
+let clickerOne = document.getElementById("img");
+let clickerTwo = document.getElementById("upgrade1-secret");
+
+switchClicker.addEventListener("click", function () {
+  if (clickerOne.style.display === "none") {
+    clickerOne.style.display = "block";
+    clickerTwo.style.display = "none";
+  } else {
+    clickerOne.style.display = "none";
+    clickerTwo.style.display = "block";
   }
 });
